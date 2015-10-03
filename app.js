9a0daacc49db579ce4d61aa1b87830e1af984d8f
Kate Wescott <wescottke@gmail.com>
2015-10-03 18:14:13 -0400

    Added Text and Fade

--- app.js
+++ app.js
@@ -7,4 +7,12 @@ var toggle = function(){
 	}else{
 		document.getElementsByClassName("dot-wrapper")[0].className = "dot-wrapper visible";
 	}
+
+	var oldtextclass = document.getElementsByClassName("text")[0].className
+	if (oldtextclass.indexOf("cancel")>-1){
+		document.getElementsByClassName("text")[0].className = "text";
+	}else{
+		document.getElementsByClassName("text")[0].className = "text cancel";	
+	}
 }
+
